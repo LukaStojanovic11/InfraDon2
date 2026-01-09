99eb52459c91bad1dfc687b6bd38809e1f1d7c15
LukaStojanovic11 <luka.stojanovic@heig-vd.ch>
2026-01-09 13:19:00 +0100

    Update db.ts
    renommage avec mon nom

--- InfraDon2nosql/src/db.ts
+++ InfraDon2nosql/src/db.ts
@@ -6,5 +6,5 @@ PouchDB.plugin(PouchDBFind)
 export const localDB = new PouchDB('infra_local')
 export const remoteDB = new PouchDB('http://admin:111911@127.0.0.1:5984/database')
 
-export const localCatDB = new PouchDB('categories_local')
-export const remoteCatDB = new PouchDB('http://admin:111911@127.0.0.1:5984/categories_db')
+export const localCatDB = new PouchDB('categories_local_Stojanovic-Luka')
+export const remoteCatDB = new PouchDB('http://admin:111911@127.0.0.1:5984/categories_db_Stojanovic-Luka')
